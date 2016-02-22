b0f15cb46bf5efb9dc22dd573f8b8a2128f1508e
Pat Wey <patrickwey@gmail.com>
2016-02-21 17:22:01 -0700

    Complete creact tutorial

---
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -0,0 +1,28 @@
+var AllSkills = React.createClass({
+
+  handleDelete(id) {
+    this.props.handleDelete(id);
+  },
+
+  onUpdate(skill) {
+    this.props.handleUpdate(skill);
+  },
+
+  render() {
+    var skills = this.props.skills.map((skill) => {
+      return (
+        <div key={skill.id}>
+          <Skill skill={skill}
+                 handleDelete={this.handleDelete.bind(this, skill.id)}
+                 handleUpdate={this.onUpdate} />
+        </div>
+      )
+    });
+
+    return (
+      <div>
+        {skills}
+      </div>
+    )
+  }
+});
---
+++ app/assets/javascripts/components/_body.js.jsx
@@ -0,0 +1,62 @@
+var Body = React.createClass({
+  getInitialState() {
+    return { skills: [] };
+  },
+
+  handleDelete(id) {
+    $.ajax({
+      url: `/api/v1/skills/${id}`,
+      type: 'DELETE',
+      success: () => {
+        this.removeSkillFromDOM(id);
+      }
+    });
+  },
+
+  removeSkillFromDOM(id) {
+    var newSkills = this.state.skills.filter((skill) => {
+      return skill.id != id;
+    });
+
+    this.setState({ skills: newSkills });
+  },
+
+  componentDidMount() {
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
+  },
+
+  handleSubmit(skill) {
+    var newState = this.state.skills.concat(skill);
+    this.setState({ skills: newState });
+  },
+
+  handleUpdate(skill) {
+    $.ajax({
+      url: `/api/v1/skills/${skill.id}`,
+      type: 'PUT',
+      data: { skill: skill },
+      success: () => {
+        console.log('you did it!');
+        this.updateSkills(skill);
+      }
+    });
+  },
+
+  updateSkills(skill) {
+    var skills = this.state.skills.filter((s) => { return s.id != skill.id });
+    skills.push(skill);
+
+    this.setState({ skills: skills });
+  },
+
+  render() {
+    return (
+      <div>
+        <NewSkill handleSubmit={this.handleSubmit} />
+        <AllSkills skills={this.state.skills}
+                   handleDelete={this.handleDelete}
+                   handleUpdate={this.handleUpdate}/>
+      </div>
+    )
+  }
+});
